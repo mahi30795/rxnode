fb9503c292f16449ced1513b48f7a4d82cac35f9
MaHi <mahesh30795@gmail.com>
2018-11-21 13:39:08 +0530

    Change in testrx.sh

--- testrx.sh
+++ testrx.sh
@@ -40,7 +40,7 @@ function setChaincodePath(){
 	LANGUAGE=`echo "$LANGUAGE" | tr '[:upper:]' '[:lower:]'`
 	case "$LANGUAGE" in
 		"golang")
-		CC_SRC_PATH="github.com/example_cc/go"
+		CC_SRC_PATH="github.com/example_cc/go/example_cc.go"
 		;;
 		"node")
 		CC_SRC_PATH="$PWD/artifacts/src/github.com/example_cc/node"
